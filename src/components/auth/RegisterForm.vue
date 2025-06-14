c57ac021fc0f11b05ed7e453a15bcfb78ff7bf00
Yuucoles <Levicoles09@gmail.com>
2025-06-14 16:56:06 +0700

    feat: added password view and format

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const isPasswordVisible = ref(false)
+const isPasswordConfirmVisible = ref(false)
+</script>
+
 <template>
     <v-form fast-fail @submit.prevent>
                     <v-row>
@@ -13,20 +20,29 @@
 
                     <v-text-field label="Email" variant="outlined" prepend-inner-icon="mdi-email-outline"></v-text-field>
                     
-                    <v-text-field 
-                        label="Password" 
-                        type="password" 
-                        variant="outlined" 
-                        prepend-inner-icon="mdi-lock-outline"> 
-                    </v-text-field>
-                    
-                    <v-text-field 
-                        label="Confirm Password" 
-                        type="password" 
-                        variant="outlined"
-                        prepend-inner-icon="mdi-lock-check-outline">
-                    </v-text-field>
-
+                    <v-row>
+                        <v-col md="6">
+                            <v-text-field 
+                                prepend-inner-icon="mdi-lock-outline"
+                                label="Password" 
+                                variant="outlined" 
+                                :type="isPasswordVisible ? 'text' : 'password'"
+                                :append-inner-icon="isPasswordVisible ? 'mdi-eye-closed' : 'mdi-eye'" 
+                                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                                ></v-text-field>
+                        </v-col>    
+                            
+                        <v-col md="6">
+                            <v-text-field 
+                                prepend-inner-icon="mdi-lock-check-outline"
+                                label="Confirm Password" 
+                                variant="outlined"
+                                :type="isPasswordConfirmVisible ? 'text' : 'password'"
+                                :append-inner-icon="isPasswordConfirmVisible ? 'mdi-eye-closed' : 'mdi-eye'" 
+                                @click:append-inner="isPasswordConfirmVisible = !isPasswordConfirmVisible"
+                                ></v-text-field>
+                        </v-col>
+                    </v-row>
                     <v-btn class="mt-2" type="submit" block color="primary" prepend-icon="mdi-account-plus">Create account</v-btn>
                   </v-form>
 </template>
